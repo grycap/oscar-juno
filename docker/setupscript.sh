8a3eefa8f80d8f932f19fbe423b309d5a2a611d4
SergioLangaritaBenitez <sergiolangaritabenitez@gmail.com>
2024-11-07 09:26:37 +0100

    add elyra

--- docker/setupscript.sh
+++ docker/setupscript.sh
@@ -1,3 +1,6 @@
 eval `oidc-agent-service restart`
 oidc-gen --pub --issuer https://aai.egi.eu/auth/realms/egi --scope "email  eduperson_entitlement eduperson_scoped_affiliation eduperson_unique_id" oscar-egi
 chmod 0777 -R  /tmp/oidc-agent-service-0 
+
+mkdir -p /var/run/secrets/egi.eu
+oidc-token oscar-egi > /var/run/secrets/egi.eu/access_token
--- script.sh
+++ script.sh
@@ -19,4 +19,28 @@ curl https://raw.githubusercontent.com/$GIT_REPO/master/examples/script_plant.sh
 
 curl https://raw.githubusercontent.com/$OSCAR_REPO/master/examples/body-pose-detection/images/001.jpg > $JUPYTER_DIRECTORY/body-pose.jpg
 curl https://raw.githubusercontent.com/$OSCAR_REPO/master/examples/plant-classification-sync/images/image1.jpg > $JUPYTER_DIRECTORY/plant.jpg
-start-notebook.sh --NotebookApp.allow_root=True  --Session.username=root  --NotebookApp.base_url=$JHUB_BASE_URL --NotebookApp.token=$JUPYTER_TOKEN  --NotebookApp.notebook_dir=$JUPYTER_DIRECTORY 
+curl https://raw.githubusercontent.com/$GIT_REPO/master/examples/oscar_tutorial.pipeline > $JUPYTER_DIRECTORY/oscar_tutorial.pipeline
+
+#curl https://raw.githubusercontent.com/grycap/apricotlab/main/apricot-tutorial.ipynb > $JUPYTER_DIRECTORY/apricot-tutorial.ipynb
+#curl https://raw.githubusercontent.com/grycap/apricotlab/main/src/apricot_magics.py > $JUPYTER_DIRECTORY/apricot_magics.py
+
+
+#git clone https://github.com/grycap/apricotlab $JUPYTER_DIRECTORY/apricotlab
+#mv /apricotlab $JUPYTER_DIRECTORY/apricotlab
+#jlpm install
+#pip install backports.tarfile
+#pip install  -ve $JUPYTER_DIRECTORY/apricotlab 
+#jupyter labextension develop --overwrite $JUPYTER_DIRECTORY/apricotlab
+
+#cp  /apricotlab/resources/ $JUPYTER_DIRECTORY -r
+
+#cp /apricotlab $JUPYTER_DIRECTORY -r
+#python3 -m pip install --upgrade pip
+#pip install backports.tarfile
+#pip install  -ve $JUPYTER_DIRECTORY/apricotlab 
+#jupyter labextension develop --overwrite $JUPYTER_DIRECTORY/apricotlab
+
+
+jlpm run build
+jupyter lab --ServerApp.allow_root=True  --Session.username=root  --ServerApp.base_url=$JHUB_BASE_URL --IdentityProvider.token=$JUPYTER_TOKEN  --ServerApp.root_dir=$JUPYTER_DIRECTORY --ip=0.0.0.0 --no-browser 
+#start-notebook.sh --ServerApp.allow_root=True  --Session.username=root  --ServerApp.base_url=$JHUB_BASE_URL --IdentityProvider.token=$JUPYTER_TOKEN  --ServerApp.root_dir=$JUPYTER_DIRECTORY 
